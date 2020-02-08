a8f81864023f9857712dcd9ecf1267c8f3e5921c
ernestpazera <ernestpazera@gmail.com>
2020-02-07 19:25:49 -0600

    initial

---
+++ Poips/board/board.go
@@ -0,0 +1,30 @@
+components {
+  id: "board"
+  component: "/board/board.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "boardScript"
+  component: "/board/boardScript.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
