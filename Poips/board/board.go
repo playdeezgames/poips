34c731ab1b7547729025e66c49b0ac3446ca7cc0
ernestpazera <ernestpazera@gmail.com>
2020-02-08 11:26:00 -0600

    shippit

--- Poips/board/board.go
+++ Poips/board/board.go
@@ -28,3 +28,48 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "music"
+  component: "/assets/audio/music.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "lock"
+  component: "/assets/audio/lock.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "turn"
+  component: "/assets/audio/turn.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
