1763c89e4b0d23c33e837dfd744733b41edc5761
ernestpazera <ernestpazera@gmail.com>
2020-02-12 06:07:06 -0600

    working towards gui

---
+++ Poips/titlebuttons/titlebuttons.go
@@ -0,0 +1,15 @@
+components {
+  id: "titlebuttons"
+  component: "/titlebuttons/titlebuttons.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
